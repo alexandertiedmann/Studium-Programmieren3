4776123358e8403dcd94fee38faddc64be21dd3d
Christoph Stach <christoph.stach@gmail.com>
2017-04-18 19:42:36 +0200

    Init

---
+++ src/test/java/edu/christophstach/AppTest.java
@@ -0,0 +1,12 @@
+package edu.christophstach;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test for App.
+ */
+public class AppTest extends TestCase {
+  public void testMain() {
+    
+  }
+}
