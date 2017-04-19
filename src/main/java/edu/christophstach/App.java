ccb1fb212bc86b8c21ac5bd94826fea146b81879
Christoph Stach <christoph.stach@gmail.com>
2017-04-19 12:35:55 +0200

    Reformating für schöneren Quellcode
    Einfach bei IntelliJ STRG-ALT-L drücken dann wird der Code sehr gut automatisch formatiert

--- src/main/java/edu/christophstach/App.java
+++ src/main/java/edu/christophstach/App.java
@@ -8,54 +8,54 @@ import edu.christophstach.bank.*;
  * Mainprogramm für bank-project
  */
 public class App {
-  /**
-   * Main
-   *
-   * @param args CMD args
-   */
-  public static void main(String[] args) {
-    Kontoart art = Kontoart.GIROKONTO;
-    System.out.println(art.name() + " " + art.ordinal());
-    art = Kontoart.valueOf("SPARBUCH");
-    System.out.println(art.name() + " " + art.ordinal());
+    /**
+     * Main
+     *
+     * @param args CMD args
+     */
+    public static void main(String[] args) {
+        Kontoart art = Kontoart.GIROKONTO;
+        System.out.println(art.name() + " " + art.ordinal());
+        art = Kontoart.valueOf("SPARBUCH");
+        System.out.println(art.name() + " " + art.ordinal());
 
-    for (int i = 0; i < Kontoart.values().length; i++) {
-      System.out.println(Kontoart.values()[i].getWerbebotschaft());
+        for (int i = 0; i < Kontoart.values().length; i++) {
+            System.out.println(Kontoart.values()[i].getWerbebotschaft());
+        }
+
+
+        int a = 50;
+        int b = a;
+        a += 50;
+        System.out.println(b); //50
+
+
+        //Kunde ich = new Kunde("Dorothea", "Hubrich", "zuhause", LocalDate.parse("1976-07-13"));
+        Student ich = new Student("Alexander", "Tiedmann", "@Home", "04.02.93", "HTW", 3);
+
+        Konto meinGiro = new Girokonto(ich, 1234, 1000.0);
+        meinGiro.einzahlen(50);
+        System.out.println(meinGiro);
+        System.out.println("-------------------------");
+        meinGiro.aufDieKonsoleSchreiben();
+
+        Konto referenz = meinGiro;
+        meinGiro.einzahlen(50);
+        System.out.println(referenz.getKontostand());
+
+        System.out.println("Nach dem Umzug:");
+        ich.setAdresse("Woanders");
+        System.out.println(meinGiro);
+
+        Konto meinSpar = new Sparbuch(ich, 9876);
+        meinSpar.einzahlen(50);
+
+        try {
+            boolean hatGeklappt = meinSpar.abheben(70);
+            System.out.println("Abhebung hat geklappt: " + hatGeklappt);
+            System.out.println(meinSpar);
+        } catch (GesperrtException e) {
+            System.out.println("Zugriff auf gesperrtes Konto - Polizei rufen!");
+        }
     }
-
-
-    int a = 50;
-    int b = a;
-    a += 50;
-    System.out.println(b); //50
-
-
-    //Kunde ich = new Kunde("Dorothea", "Hubrich", "zuhause", LocalDate.parse("1976-07-13"));
-    Student ich = new Student("Alexander", "Tiedmann", "@Home", "04.02.93","HTW",3);
-
-    Konto meinGiro = new Girokonto(ich, 1234, 1000.0);
-    meinGiro.einzahlen(50);
-    System.out.println(meinGiro);
-    System.out.println("-------------------------");
-    meinGiro.aufDieKonsoleSchreiben();
-
-    Konto referenz = meinGiro;
-    meinGiro.einzahlen(50);
-    System.out.println(referenz.getKontostand());
-
-    System.out.println("Nach dem Umzug:");
-    ich.setAdresse("Woanders");
-    System.out.println(meinGiro);
-
-    Konto meinSpar = new Sparbuch(ich, 9876);
-    meinSpar.einzahlen(50);
-    
-    try {
-      boolean hatGeklappt = meinSpar.abheben(70);
-      System.out.println("Abhebung hat geklappt: " + hatGeklappt);
-      System.out.println(meinSpar);
-    } catch (GesperrtException e) {
-      System.out.println("Zugriff auf gesperrtes Konto - Polizei rufen!");
-    }
-  }
 }
--- src/test/java/edu/christophstach/bank/StudentTest.java
+++ src/test/java/edu/christophstach/bank/StudentTest.java
@@ -1,44 +1,46 @@
-import edu.christophstach.bank.Student;
+package edu.christophstach.bank;
+
 import junit.framework.TestCase;
 
 /**
  * Test fuer Student
+ *
  * @author Alexander Tiedmann (s0556127)
  * @version 1.0
  * @since 18.04.2017
  */
 public class StudentTest extends TestCase {
-    Student s1,s2;
+    Student s1, s2;
 
-    protected void setUp(){
-        s1 = new Student("Test","Test", "Teststr","2000-01-01","HTW",3);
-        s2 = new Student("Test","Test", "Teststr","2000-01-02","HTW",0);
+    protected void setUp() {
+        s1 = new Student("Test", "Test", "Teststr", "2000-01-01", "HTW", 3);
+        s2 = new Student("Test", "Test", "Teststr", "2000-01-02", "HTW", 0);
     }
 
-    public void testIstVerguenstigt(){
+    public void testIstVerguenstigt() {
         assertTrue(s1.istVerguenstigt());
     }
 
-    public void testIstVerguenstigtFail(){
+    public void testIstVerguenstigtFail() {
         assertFalse(s2.istVerguenstigt());
     }
 
-    public void testNeueBescheinigung(){
+    public void testNeueBescheinigung() {
         boolean ok = true;
         try {
             s1.neueBescheinigung(4);
-        }catch (ArithmeticException e){
+        } catch (ArithmeticException e) {
             ok = false;
         }
         assertTrue(ok);
         assertTrue(s1.getSemester() == 4);
     }
 
-    public void testNeueBescheinigungFail(){
+    public void testNeueBescheinigungFail() {
         boolean ok = true;
         try {
             s1.neueBescheinigung(2);
-        }catch (ArithmeticException e){
+        } catch (ArithmeticException e) {
             ok = false;
         }
         assertFalse(ok);
